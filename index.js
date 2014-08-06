59f79cab2a446284060038d9d7bd91b043db4853
Jason Sommer <jsdirv@gmail.com>
2014-08-06 01:15:26 -0500

    Stubbed methods

---
+++ index.js
@@ -0,0 +1,12 @@
+function convertSchema(schema) {
+  // Convert to cql.
+}
+
+function convertDelta(delta) {
+  // Convert to cql.
+}
+
+module.exports = {
+  convertSchema: convertSchma,
+  convertDelta: convertDelta
+};
